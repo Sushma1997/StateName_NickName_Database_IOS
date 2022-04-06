f80a7e9d8f4fd14bac07ed51c3f3974c871dc296
sushma Reddy T <sushee.reddy1997@gmail.com>
2022-04-05 21:08:30 -0500

    added url to view controller

--- Ind4_TambiReddy_SushmaReddy/ViewController.swift
+++ Ind4_TambiReddy_SushmaReddy/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  Ind4_TambiReddy_SushmaReddy
-//
-//  Created by Sushma Reddy on 4/5/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fetchState = [States]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        parseData()
     }
-
+    
+    func parseData() {
+        
+        fetchState = []
+        let url = "https://cs.okstate.edu/~stambir/states.php"
+        var request = URLRequest(url: URL(string:url)!)
+        request.httpMethod = "GET"
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if (error != nil) {
+                print("Error")
+            }
+            else {
+                do {
+                    let fetchData = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSArray
+                    for eachFetchState in fetchData {
+                        
+                        let eachState = eachFetchState as! [String : Any]
+                        let statename = eachState["statename"] as! String
+                        let nickname = eachState["nickname"] as! String
+                        
+                        self.fetchState.append(States(statename : statename, nickname : nickname))
+                    }
+                    print(self.fetchState)
+                }
+                catch{
+                    print("Error2")
+                }
+            }
+        }
+    task.resume()
+}
 
 }
 
+class States {
+    var statename : String
+    var nickname : String
+    
+    init(statename : String , nickname : String) {
+        self.statename = statename
+        self.nickname = nickname
+    }
+}
